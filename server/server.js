1cfdf867e7cde6c9a0b4a08ed08a378d5346fa55
RiyaMaheshwari <riyamaheshwari9635@gmail.com>
2025-10-19 00:00:48 +0530

    database has been successfully connected

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import "dotenv/config";
+import { connectDB } from "./lib/database.js";
+
+//Create express app and HTTP server
+const app = express();
+const server = http.createServer(app);
+
+//Middleware setup
+app.use(express.json({limit: "4mb"}))
+app.use(cors());
+
+app.use("/api/status", (req, res)=> res.send("Server is live"));
+
+//connect to mongoDB
+await connectDB();
+
+const PORT = process.env.PORT || 2000;
+
+server.listen(PORT, ()=> console.log("Server is running on", + PORT));
